06a8faae717af198f96fd62422752492667a2b47
Hyunwoo Choi <chw64233@gmail.com>
2023-09-12 16:19:56 +0900

    Initial commit

---
+++ src/test/java/leave/meet/playbours/PlayboursApplicationTests.java
@@ -0,0 +1,13 @@
+package leave.meet.playbours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayboursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
