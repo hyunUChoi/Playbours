5b7e4ba8def9f87a3bf99d60705100fa3a69aa76
Hyunwoo Choi <chw64233@gmail.com>
2023-09-12 16:19:56 +0900

    Initial commit

---
+++ src/main/java/leave/meet/playbours/PlayboursApplication.java
@@ -0,0 +1,13 @@
+package leave.meet.playbours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayboursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayboursApplication.class, args);
+    }
+
+}
