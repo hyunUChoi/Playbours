acb66e6bb904834de6af162ec2979b69d2c4ea39
hyunUChoi <chw64233@gmail.com>
2023-09-14 15:19:21 +0900

    최현우 - css icon 오류 수정, 관리자 layout, aside, html 수정

--- src/main/java/leave/meet/playbours/controller/manage/ManageHomeController.java
+++ src/main/java/leave/meet/playbours/controller/manage/ManageHomeController.java
@@ -10,4 +10,50 @@ public class ManageHomeController {
     public String maHome() {
         return "pages/manage/index";
     }
+
+    @RequestMapping("/icon")
+    public String icon() {
+        return "pages/manage/icon-tabler";
+    }
+
+    @RequestMapping("/sample")
+    public String sample() {
+        return "pages/manage/sample-page";
+    }
+
+    @RequestMapping("/alert")
+    public String alert() {
+        return "pages/manage/ui-alerts";
+    }
+
+    @RequestMapping("/button")
+    public String button() {
+        return "pages/manage/ui-buttons";
+    }
+
+    @RequestMapping("/card")
+    public String card() {
+        return "pages/manage/ui-card";
+    }
+
+    @RequestMapping("/form")
+    public String form() {
+        return "pages/manage/ui-forms";
+    }
+
+    @RequestMapping("/ty")
+    public String ty() {
+        return "pages/manage/ui-typography";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "pages/manage/authentication-login";
+    }
+
+    @RequestMapping("/register")
+    public String register() {
+        return "pages/manage/authentication-register";
+    }
+
 }
