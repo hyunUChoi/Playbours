2b07ade3ad92ff480052976ab136953f24b19c93
GOGOYS <qaz75391@naver.com>
2023-12-07 16:04:20 +0900

    유저게시판 form 작성중

--- src/main/java/leave/meet/playbours/manage/member/user/repository/MaUserRepository.java
+++ src/main/java/leave/meet/playbours/manage/member/user/repository/MaUserRepository.java
@@ -6,4 +6,18 @@ import org.springframework.data.domain.Page;
 public interface MaUserRepository {
 
     Page<MaUserDto> findByPagingAndFiltering(int page, int size, MaUserDto maUserDto);
+
+    int countTotalRecords(MaUserDto dto);
+
+    MaUserDto findOne(MaUserDto dto);
+
+    void insert(MaUserDto dto);
+
+    void update(MaUserDto dto);
+
+    void updateReply(MaUserDto dto);
+
+    void delete(MaUserDto dto);
+
+    void deleteReply(MaUserDto dto);
 }
--- src/main/java/leave/meet/playbours/manage/member/user/repository/impl/MaUserRepositoryImpl.java
+++ src/main/java/leave/meet/playbours/manage/member/user/repository/impl/MaUserRepositoryImpl.java
@@ -2,6 +2,7 @@ package leave.meet.playbours.manage.member.user.repository.impl;
 
 import leave.meet.playbours.manage.member.user.service.MaUserDto;
 import leave.meet.playbours.manage.member.user.repository.MaUserRepository;
+import leave.meet.playbours.manage.support.board.service.MaBoardDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -32,30 +33,7 @@ public class MaUserRepositoryImpl implements MaUserRepository {
                 .limit(pageable.getPageSize());
 
         query.addCriteria(Criteria.where("delYn").is("N"));
-
-        /* 쿼리 조건 */
-        if(dto.getSearch1() != null && !"".equals(dto.getSearch1() )) {
-            query.addCriteria(Criteria.where("menuClCd").is(dto.getSearch1()));
-        }
-
-        if(dto.getSearchKeyword() != null && !"".equals(dto.getSearchKeyword())) {
-            if(dto.getSearchOption() != null && !"".equals(dto.getSearchOption())) {
-                switch (dto.getSearchOption()) {
-                    case "0" -> {
-                        Criteria criteria = new Criteria();
-                        criteria.orOperator(Criteria.where("menuCd").is(dto.getSearchKeyword()), Criteria.where("menuNm").is(dto.getSearchKeyword()));
-                        query.addCriteria(criteria);
-                    }
-                    case "1" -> {
-                        query.addCriteria(Criteria.where("menuNm").is(dto.getSearchKeyword()));
-                    }
-                    case "2" -> {
-                        query.addCriteria(Criteria.where("menuCd").is(dto.getSearchKeyword()));
-                    }
-                }
-            }
-        }
-
+        searchCondition(query, dto);
         query.with(Sort.by(Sort.Direction.DESC, "seq"));
 
         List<MaUserDto> filterData = mongoTemplate.find(query, MaUserDto.class);
@@ -63,5 +41,78 @@ public class MaUserRepositoryImpl implements MaUserRepository {
         return PageableExecutionUtils.getPage(
                 filterData, pageable, () -> mongoTemplate.count(query.skip(-1).limit(-1), MaUserDto.class)
         );
+    }
+
+    /* 검색 조건문 */
+    public Query searchCondition(Query query, MaUserDto dto) {
+
+        /* 날짜 조건 */
+        if(dto.getSearchStrDt() != null) {
+            query.addCriteria(Criteria.where("notiStrDt").gte(dto.getSearchStrDt()));
+        }
+        if(dto.getSearchEndDt() != null) {
+            query.addCriteria(Criteria.where("notiEndDt").lte(dto.getSearchEndDt()));
+        }
+
+        if(dto.getSearchKeyword() != null && !"".equals(dto.getSearchKeyword())) {
+            if(dto.getSearchOption() != null && !"".equals(dto.getSearchOption())) {
+                switch (dto.getSearchOption()) {
+                    case "0" -> {
+                        Criteria criteria = new Criteria();
+                        criteria = criteria.orOperator(Criteria.where("title").is(dto.getSearchKeyword()), Criteria.where("cont").is(dto.getSearchKeyword()));
+                        query.addCriteria(criteria);
+                    }
+                    case "1" -> {
+                        query.addCriteria(Criteria.where("title").is(dto.getSearchKeyword()));
+                    }
+                    case "2" -> {
+                        query.addCriteria(Criteria.where("cont").is(dto.getSearchKeyword()));
+                    }
+                }
+            }
+        }
+
+        return query;
+    }
+
+    @Override
+    public int countTotalRecords(MaUserDto dto) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("delYn").is("N"));
+        searchCondition(query, dto);
+        return (int) mongoTemplate.count(query, MaUserDto.class);
+    }
+
+    @Override
+    public MaUserDto findOne(MaUserDto dto) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("seq").is(dto.getSeq()));
+        query.addCriteria(Criteria.where("delYn").is("N"));
+        return mongoTemplate.findOne(query, MaUserDto.class);
+    }
+
+    @Override
+    public void insert(MaUserDto dto) {
+
+    }
+
+    @Override
+    public void update(MaUserDto dto) {
+
+    }
+
+    @Override
+    public void updateReply(MaUserDto dto) {
+
+    }
+
+    @Override
+    public void delete(MaUserDto dto) {
+
+    }
+
+    @Override
+    public void deleteReply(MaUserDto dto) {
+
     }
 }
